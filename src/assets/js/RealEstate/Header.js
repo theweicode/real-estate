b2db49473090a3c7be9e6467ccf21fc0f952d3eb
theweicode <theweicode@gmail.com>
2019-05-02 15:33:12 -0700

    login and register with firebase auth added

--- src/assets/js/RealEstate/Header.js
+++ src/assets/js/RealEstate/Header.js
@@ -6,33 +6,49 @@ import firebase from "firebase";
 class Header extends Component {
   constructor() {
     super();
-    this.handleShow = this.handleShow.bind(this);
-    this.handleClose = this.handleClose.bind(this);
+    this.showLogin = this.showLogin.bind(this);
+    this.hideLogin = this.hideLogin.bind(this);
     this.showSignUp = this.showSignUp.bind(this);
     this.hideSignUp = this.hideSignUp.bind(this);
+    this.handleGuestLogin = this.handleGuestLogin.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleSignUp = this.handleSignUp.bind(this);
 
     this.state = {
-      show: false,
-      showSignUp: false
+      showSignUp: true,
+      showLogin: false,
+      guestLogin: false,
+      username: "",
+      email: "",
+      password: "",
+      loginError: ""
     };
   }
-  handleClose() {
-    this.setState({ show: false });
-    console.log("handleclose clicked");
+
+  handleChange(evt) {
+    this.setState({ [evt.target.name]: evt.target.value });
+    console.log(this.state.email);
+    console.log(this.state.password);
   }
 
-  handleShow() {
-    this.setState({ show: true });
-  }
-
+  //navbar modal controls
+  // login
   showSignUp() {
     this.setState({ showSignUp: true });
   }
   hideSignUp() {
     this.setState({ showSignUp: false });
   }
+  showLogin() {
+    this.setState({ showLogin: true });
+  }
 
-  handleAnonSI() {
+  hideLogin() {
+    this.setState({ showLogin: false });
+  }
+
+  handleGuestLogin() {
     firebase
       .auth()
       .signInAnonymously()
@@ -43,20 +59,23 @@ class Header extends Component {
         // ...
       });
 
-    firebase.auth().onAuthStateChanged(function(user) {
-      if (user) {
-        // User is signed in.
-        var isAnonymous = user.isAnonymous;
-        var uid = user.uid;
-        var self = this;
-        self.setState({ show: false });
+    firebase.auth().onAuthStateChanged(
+      function(user) {
+        if (user) {
+          // User is signed in.
+          var isAnonymous = user.isAnonymous;
+          var uid = user.uid;
+          this.setState({ username: "Guest" });
+          this.setState({ guestLogin: true });
+          console.log(this.state.username);
+          // ...
+        } else {
+          // User is signed out.
+          // ...
+        }
         // ...
-      } else {
-        // User is signed out.
-        // ...
-      }
-      // ...
-    });
+      }.bind(this)
+    );
   }
   handleEMSI() {
     firebase
@@ -84,52 +103,191 @@ class Header extends Component {
     });
   }
 
+  handleSignUp(e) {
+    e.preventDefault();
+    let email = this.state.email;
+    let password = this.state.password;
+
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch(
+        function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // ...
+          this.setState({ loginError: errorMessage });
+        }.bind(this)
+      )
+      .then(this.setState({ loginError: "Account Successfully Created" }));
+    document.getElementById("signupform").reset();
+    document.getElementById("passwordfield").reset();
+  }
+  handleLogin() {
+    let email = this.state.email;
+    let password = this.state.password;
+
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch(
+        function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // ...
+          this.setState({ loginError: errorMessage });
+        }.bind(this)
+      );
+  }
+
   render() {
+    let logUsr;
+    let loginEr = this.state.loginError;
+    if (this.state.guestLogin) {
+      logUsr = <h3>Logged in as: {this.state.username}</h3>;
+    }
     return (
       <>
         <header>
           <div className="logo">
             <img src="https://i.imgur.com/1fXUQ8a.png" />
           </div>
+          {logUsr}
+
           <nav>
             <a href="#">Create Ads</a>
             <a href="#">About Us</a>
-            <a onClick={this.handleShow}>Log In</a>
+            <a onClick={this.showLogin}>Log In</a>
             <a onClick={this.showSignUp} className="register-btn">
               Register
             </a>
           </nav>
         </header>
-        <Modal show={this.state.show} onHide={this.handleClose}>
+        <Modal show={this.state.showLogin} onHide={this.hideLogin}>
           <Modal.Header closeButton>
-            <Modal.Title>Register Sign In</Modal.Title>
+            <Modal.Title>Log In</Modal.Title>
           </Modal.Header>
-          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-          <Button onClick={this.handleAnonSI}>Sign In Anonymously</Button>
-          <Button onClick={this.handleEMSI}>Sign In w/ Email</Button>
+          <Modal.Body>Enter your login info here:</Modal.Body>
+
+          <div className="input-group mb-3">
+            <div className="input-group-prepend">
+              <span className="input-group-text" id="inputGroup-sizing-default">
+                Email
+              </span>
+            </div>
+            <input
+              type="text"
+              className="form-control"
+              aria-label="Sizing example input"
+              aria-describedby="inputGroup-sizing-default"
+              name="email"
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div className="input-group mb-3">
+            <div className="input-group-prepend">
+              <span className="input-group-text" id="inputGroup-sizing-default">
+                Password
+              </span>
+            </div>
+            <input
+              type="text"
+              className="form-control"
+              aria-label="Sizing example input"
+              aria-describedby="inputGroup-sizing-default"
+              name="password"
+              onChange={this.handleChange}
+            />
+          </div>
+          {loginEr}
+          <Button variant="primary" onClick={this.handleLogin}>
+            Login
+          </Button>
+
           <Modal.Footer>
-            <Button variant="secondary" onClick={this.handleClose}>
-              Close
+            <Button
+              variant="dark"
+              onClick={event => {
+                this.hideLogin();
+                this.handleGuestLogin();
+              }}
+            >
+              Login As Guest
             </Button>
-            <Button variant="primary" onClick={this.handleClose}>
-              Save Changes
+            <Button variant="secondary" onClick={this.hideLogin}>
+              Close
             </Button>
           </Modal.Footer>
         </Modal>
 
         <Modal show={this.state.showSignUp} onHide={this.hideSignUp}>
           <Modal.Header closeButton>
-            <Modal.Title>Register Sign In</Modal.Title>
+            <Modal.Title>Sign Up</Modal.Title>
           </Modal.Header>
-          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-          <Button onClick={this.handleAnonSI}>Sign In Anonymously</Button>
-          <Button onClick={this.handleEMSI}>Sign In w/ Email</Button>
+          <Modal.Body>Enter your login info here:</Modal.Body>
+
+          <div className="input-group mb-3">
+            <div className="input-group-prepend">
+              <span className="input-group-text" id="inputGroup-sizing-default">
+                Email
+              </span>
+            </div>
+            <form id="signupform">
+              <input
+                type="text"
+                onKeyPress={e => {
+                  if (e.key === "Enter") e.preventDefault();
+                }}
+                className="form-control"
+                aria-label="Sizing example input"
+                aria-describedby="inputGroup-sizing-default"
+                name="email"
+                onChange={this.handleChange}
+              />
+            </form>
+          </div>
+
+          <div className="input-group mb-3">
+            <div className="input-group-prepend">
+              <span className="input-group-text" id="inputGroup-sizing-default">
+                Password
+              </span>
+            </div>
+
+            <form id="passwordfield">
+              <input
+                type="text"
+                onKeyPress={e => {
+                  if (e.key === "Enter") e.preventDefault();
+                }}
+                className="form-control"
+                aria-label="Sizing example input"
+                aria-describedby="inputGroup-sizing-default"
+                name="password"
+                onChange={this.handleChange}
+              />
+            </form>
+          </div>
+          {loginEr}
+          <Button variant="primary" onClick={this.handleSignUp}>
+            Create Account
+          </Button>
+
           <Modal.Footer>
-            <Button variant="secondary" onClick={this.hideSignUp}>
-              Close
+            <Button
+              variant="dark"
+              onClick={event => {
+                this.hideLogin();
+                this.handleGuestLogin();
+              }}
+            >
+              Login As Guest
             </Button>
-            <Button variant="primary" onClick={this.hideSignUp}>
-              Save Changes
+            <Button variant="secondary" onClick={this.hideLogin}>
+              Close
             </Button>
           </Modal.Footer>
         </Modal>
